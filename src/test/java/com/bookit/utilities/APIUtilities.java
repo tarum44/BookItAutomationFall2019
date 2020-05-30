4ce758bc3cd2a21637ab5d8e42b2deaf141d561c
muratkilinc <tarum44@gmail.com>
2020-05-30 14:44:29 -0500

    updates

---
+++ src/test/java/com/bookit/utilities/APIUtilities.java
@@ -0,0 +1,56 @@
+package com.bookit.utilities;
+
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
+
+public class APIUtilities {
+
+    static {
+        baseURI = Environment.BASE_URI;
+    }
+
+
+    /**
+     * This method is used to retrieve token from the server
+     * Token bust be attached to the header of every API call
+     * @return
+     */
+
+    public static String getToken(){
+        Response response=given().
+                queryParam("email",Environment.LEADER_USERNAME).
+                queryParam("password",Environment.LEADER_USERNAME).
+                when().
+                get("/sign");
+        response.then().log().ifError(); // if request failed, print response information
+        String token=response.jsonPath().getString("accessToken");
+        System.out.println("TOKEN" +token);
+        return token;
+    }
+
+    public static String getToken(String role){
+        String email=null;
+        String password=null;
+        if(role.toLowerCase().contains("teacher")){
+            email=Environment.TEACHER_USERNAME;
+            password=Environment.TEACHER_PASSWORD;
+        }else if(role.toLowerCase().contains("lead")){
+            email=Environment.LEADER_USERNAME;
+            password=Environment.LEADER_PASSWORD;
+        }else{
+            email=Environment.MEMBER_USERNAME;
+            password=Environment.MEMBER_PASSWORD;
+        }
+
+        Response response=given().
+                queryParam("email",Environment.LEADER_USERNAME).
+                queryParam("password",Environment.LEADER_USERNAME).
+                when().
+                get("/sign");
+        response.then().log().ifError(); // if request failed, print response information
+        String token=response.jsonPath().getString("accessToken");
+        System.out.println("TOKEN" +token);
+        return token;
+    }
+}
